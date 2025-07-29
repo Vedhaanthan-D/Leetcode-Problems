cfead0547f5b426454a63e2228abb50d79de47f0
Vedhaanthan-D <vedhaanthan2006@gmail.com>
2025-07-29 11:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (65.69%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str=s.trim();
+        int c=0;
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)==' '){
+                break;
+            }
+            else{
+                c++;
+            }
+        }
+        return c;
+    }
+}
