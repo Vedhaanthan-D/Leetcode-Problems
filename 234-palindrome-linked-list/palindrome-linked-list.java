51a0ffed5afcdc4eebaf3a1c121a03b0e5224b0a
Vedhaanthan D <vedhaanthan2006@gmail.com>
2026-07-13 10:09:13 +0530

    Time: 5 ms (44.26%) | Memory: 94.3 MB (67.53%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,30 +1,63 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
+ *     int val;ext;
+ *     ListNode n
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
+    public ListNode findmid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
     public boolean isPalindrome(ListNode head) {
-        List<Integer> arr=new ArrayList<>();
+        // List<Integer> arr=new ArrayList<>();
 
-        while(head!=null){
-            arr.add(head.val);
-            head=head.next;
-        }   
-        int left=0;
-        int right=arr.size()-1;
+        // while(head!=null){
+        //     arr.add(head.val);
+        //     head=head.next;
+        // }   
+        // int left=0;
+        // int right=arr.size()-1;
 
-        while(left<right){
-            if(!arr.get(left).equals(arr.get(right))){
+        // while(left<right){
+        //     if(!arr.get(left).equals(arr.get(right))){
+        //         return false;
+        //     }
+        //     left++;
+        //     right--;
+        // }
+        // return true;
+
+        ListNode mid=findmid(head);
+        ListNode prev=null;
+        ListNode curr=mid.next;
+
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        ListNode right=prev;
+        ListNode left=head;
+
+        while(right!=null){
+            if(left.val!= right.val){
                 return false;
             }
-            left++;
-            right--;
+            left=left.next;
+            right=right.next;
         }
         return true;
     }
